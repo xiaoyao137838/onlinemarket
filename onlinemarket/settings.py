0cf0b813841d868164282abb41296bdefacef7db
xiaoyao137838 <hongyao.geng@gmail.com>
2023-07-26 09:27:17 -0400

    Update gdal in settings

--- onlinemarket/settings.py
+++ onlinemarket/settings.py
@@ -184,9 +184,9 @@ MAPBOX_TOKEN = config('MAPBOX_TOKEN')
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
 if DEBUG == True:
-    os.environ['PATH'] = os.path.join(BASE_DIR, '..\env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
-    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, '..\env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
-    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, '..\env\Lib\site-packages\osgeo\gdal304.dll')
+    os.environ['PATH'] = os.path.join(BASE_DIR, '\env\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, '\env\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, '\env\Lib\site-packages\osgeo\gdal304.dll')
 
 LOGGING = {
     "version": 1,  # the dictConfig format version
